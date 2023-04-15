d9645b6bb254efa9726822ffc8a43bc014dadc4e
koshelvadim <koshelvadim@mail.ru>
2023-04-15 20:23:46 +0300

    решил третье задание

---
+++ lesson3/home3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* принимаем на вход число N и выводим таблицу кубов чисел от 1 до N */
+
+int number = ReadInt("N");
+GetPow(number);
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Введите число {argument}: ");
+    int i;
+
+    while (!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число");
+    }
+    return i;
+}
+
+void GetPow(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        System.Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
