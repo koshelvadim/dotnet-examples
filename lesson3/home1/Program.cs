fa07b72d5f58a8bede05bc44228c75682a8a1e4d
koshelvadim <koshelvadim@mail.ru>
2023-04-15 15:41:40 +0300

    решил первое задание

---
+++ lesson3/home1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* принимаем пятизначное число и проверяем, является ли оно палиндромом */
+int number = ReadInt("число");
+string num = number.ToString();
+System.Console.WriteLine(GetPalindrom(num));
+
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Введите пятизначное {argument}: ");
+    int i;
+
+    while (!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число");
+    }
+    if (i > 99999 || i < 10000)
+    {
+        System.Console.WriteLine("Это не пятизначное число");
+    }
+    return i;
+}
+
+string GetPalindrom(string num)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+        return "Да";
+    else
+        return "Нет";
+}
