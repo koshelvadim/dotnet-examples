76b3a606bd05f55f5332abb5ae7f4815317266c9
koshelvadim <koshelvadim@mail.ru>
2023-04-19 15:46:12 +0300

    выполнил доп задание прямоугольник-квадрат

---
+++ lesson4/home4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Пользователь вводит стороны прямоугольника и квадрата, проверить можно ли вписать квадрат в прямоугольник
+*/
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Введите {argument}: ");
+    int i;
+
+    while (!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число");
+    }
+    return i;
+}
+
+void GetCheck(int a, int b, int c)
+{
+    if (c <= a && c <= b) System.Console.WriteLine($"Квадрат со стороной {c} можно вписать в прямоугольник со сторонами {a} и {b}");
+    else System.Console.WriteLine($"Квадрат со стороной {c} нельзя вписать в прямоугольник со сторонами {a} и {b}");
+}
+
+int lengthA = ReadInt("длина прямоугольника");
+int lengthB = ReadInt("ширина прямоугольника");
+int lengthC = ReadInt("сторона квадрата");
+
+GetCheck(lengthA, lengthB, lengthC);
+
