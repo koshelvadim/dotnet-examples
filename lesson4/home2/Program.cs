45f22eae0025e54af17d43de90b9997eb97beb66
koshelvadim <koshelvadim@mail.ru>
2023-04-19 10:09:16 +0300

    решил 2 задание

---
+++ lesson4/home2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задание 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int number = ReadInt("number");
+int sumElement = GetSumElement(number);
+System.Console.WriteLine(sumElement);
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Please, enter {argument}: ");
+    int num;
+    while (!int.TryParse(System.Console.ReadLine(), out num))
+    {
+        System.Console.WriteLine("It's not a number");
+    }
+    return num;
+}
+
+int GetSumElement(int number)
+{
+    int sum = 0;
+    int length = number.ToString().Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
