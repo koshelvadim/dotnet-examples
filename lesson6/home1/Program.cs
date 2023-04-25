bfc0c8a9f198235d6992d27a0e04c4225285379e
koshelvadim <koshelvadim@mail.ru>
2023-04-25 23:05:20 +0300

    выполнил первое задание

---
+++ lesson6/home1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int ReadInt(string argument)
+{
+    System.Console.Write($"Введите {argument}: ");
+    int i;
+
+    while (!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число");
+    }
+    return i;
+}
+
+int[] ReadIntNumber(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = ReadInt("число");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int CheckNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int M = ReadInt("количество чисел");
+int[] arr = ReadIntNumber(M);
+PrintArray(arr);
+System.Console.WriteLine($"Пользователь ввел {CheckNumber(arr)} чисел > 0");
+
+
